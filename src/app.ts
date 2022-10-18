9a0eb619811a92955d8a5838856bef0efd1b5ade
Gabriel <gabrielcncopa@gmail.com>
2022-10-18 14:34:31 -0300

    fix app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
+import { join } from 'path';
 import { router } from './routes';
 import db from './config/mongo'
 const PORT = process.env.PORT || 3001
@@ -10,6 +11,11 @@ app.use( cors() )
 app.use( express.json() )
 
 app.use(router)
+
+app.use( express.static(join(__dirname, "../static")) )
+// app.get('/*', (req, res) => {
+//     re
+// })
 
 db().then(() => console.log('conexion ready a mongoDB'))
 
